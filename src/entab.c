d1725a654c363ce0b940ae5d0fbf6814da2d0a9f
Murygin Maksim <mail@muryginm.com>
2018-05-10 11:32:29 +0700

    update

--- src/entab.c
+++ src/entab.c
@@ -6,25 +6,56 @@ single blank would suffice to reach a tab stop, which should be given preference
 */
 
 #include <stdio.h>
+#include <stdbool.h>
 #include "include/string.h"
 
 #define MAX_LINE_SIZE 200
 #define SPACES_IN_TAB 4
 
-void entab(char * str);
+char * entab(char * str, char * entabed);
 
 int main() {
     char str[MAX_LINE_SIZE] = { 0 };
+    char entabed_str[MAX_LINE_SIZE] = { 0 };
 
     int line_length = 0;
     while ((line_length = m_getline(str, MAX_LINE_SIZE)) != EOF) {
-        entab(str);
-        printf("%s\n", str);
+        entab(str, entabed_str);
+        printf("%s\n", entabed_str);
     }
 
     return 0;
 }
 
-void entab(char * str) {
+char * entab(char * str, char * entabed) {
+    bool spaces_stopped = false;
+    int total_spaces = 0;
+    int total_tabs = 0;
+    int i = 0;
+    while (true) {
+        char c = str[i];
+        if (c == '\0') {
+            entabed[i - total_spaces + total_tabs] = '\0';
+            break;
+        }
 
+        if (!spaces_stopped && c == ' ') {
+            ++total_spaces;
+            if (total_spaces % SPACES_IN_TAB == 0) {
+                entabed[total_tabs++] = '\t';
+            }
+        } else {
+            if (!spaces_stopped) {
+                spaces_stopped = true;
+
+                if (total_spaces % SPACES_IN_TAB != 0) {
+                    entabed[total_tabs++] = '\t';
+                }
+            }
+
+            entabed[i - total_spaces + total_tabs] = str[i];
+        }
+
+        ++i;
+    }
 }
