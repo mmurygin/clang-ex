57f16ed19ec1bceccf620223c75f8a0cf448db88
Murygin Maksim <mail@muryginm.com>
2018-05-12 15:51:39 +0700

    update

---
+++ src/wrap.c
@@ -0,0 +1,16 @@
+/*
+Exercise 1-22. Write a program to “fold” long input lines into two or more shorter lines after
+the last non-blank character that occurs before the n-th column of input.
+*/
+
+#include <stdio.h>
+#include "include/string.h"
+
+#define WRAP_SIZE = 95
+#define MAX_LINE_SIZE 1000
+
+int main() {
+    /*code*/
+
+    return 0;
+}
