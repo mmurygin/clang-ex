ec7ee345b3ab2d0f98a2654c1e64270e8a18e38f
Maksim Murygin <mail@muryginm.com>
2018-05-13 12:47:13 +0700

    update

---
+++ src/squeeze.c
@@ -0,0 +1,13 @@
+/*
+Write an alternate version of squeeze(s1,s2) that deletes each character in s1 that matches any
+character in the string s2.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    /*code*/
+
+    return 0;
+}
